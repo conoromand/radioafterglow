77a174aa50676f2afb4b3a753fa405dae2fbc6ce
kazumi kashiyama <k.kashiyama1120@gmail.com>
2019-04-23 17:23:01 +0900

    add syn spec

--- radioafterglow/offaxis.c
+++ radioafterglow/offaxis.c
@@ -17,6 +17,7 @@ const double K_B = 1.3806e-16; /* [erg/K] */
 const double ELEC = 4.80320427e-10; /* [cgs] */
 const double M_PRO = 1.6726219e-24; /* [g] */
 const double M_ELE = 9.10938356e-28; /* [g] */
+const double MeC2 = 8.18709e-7; /* electron mass [erg] */
 
 const double H_0 = 67.74*1.0e5/1.0e6; /* [cm/s/pc] */ 
 const double OMEGA_M = 0.3089;
@@ -27,21 +28,20 @@ const double M_SUN = 1.989e33; /* [g]  */
 const double PC = 3.086e18; /* [cm] */
 const double YR = 365.0*24.0*60.0*60.0; /* [s]  */
 
-const double lam_obs_U = 0.3652e-4; /* [cm] */
-const double lam_obs_V = 0.4448e-4; /* [cm] */
-const double lam_obs_B = 0.5505e-4; /* [cm] */
-const double lam_obs_R = 0.6588e-4; /* [cm] */
-const double lam_obs_H = 1.654e-4; /* [cm] */
-const double lam_obs_K = 2.179e-4; /* [cm] */
-const double lam_obs_L = 3.547e-4; /* [cm] */
+const double GAMMA13 = 2.67893; /* Gamma(1/3) */
+
+const int N_ne = 256;
+const int N_nph = 256; /* need to be the same as N_ne */
 
 
 double theta_ana(double r, double a, double b, double k);
 double gam_ana(double r, double a, double b, double k);
 void conical_model(double k, double A, double a, double b, double z, double theta_j_0, double gam_j_0);
 void shock_acc_conical(double k, double A, double z, double theta_j_0, double E_j_0, double eps_B, double eps_e, double p);
-void sync_specpara_conical(double z, double d_l, double k, double p);
+void sync_spec_para_conical(double z, double d_l, double k, double p);
 double sync_spec_conical(double nu, double nu_m, double nu_c, double F_nu_max, double gam_j, double B_f, double R_perp, double p, double z, double d_l);
+double syn_func_fit(double x);
+void syn_spec(double B, double ne[], double gamma_e[], double dene_e[], double del_ln_gamma_e, double mu[], double del_mu, double gamma_ph[], double P_nu_syn[], double alpha_nu_syn[]);
 void distances(double z, double *d_h, double *d_c, double *d_a, double *d_l);
 
 int main()
@@ -75,7 +75,7 @@ int main()
 
   conical_model(k,A,a,b,z,theta_j_0,gam_j_0);
   shock_acc_conical(k,A,z,theta_j_0,E_j_0,eps_B,eps_e,p);
-  sync_specpara_conical(z,d_l,k,p);
+  sync_spec_para_conical(z,d_l,k,p);
 
   return 0;
 }
@@ -153,8 +153,8 @@ void shock_acc_conical(double k, double A, double z, double theta_j_0, double E_
     R_perp[i] = R_j*r[i]*(2.0*theta_j[i]-sin(2.0*theta_j[i]))/2.0/M_PI/(1.0-cos(theta_j[i]));
     R_para[i] = R_j*r[i]*sin(theta_j[i])*sin(theta_j[i])/2.0/(1.0-cos(theta_j[i]));
     theta_ene_ave[i] = (sin(theta_j[i])-theta_j[i]*cos(theta_j[i]))/(1.0-cos(theta_j[i]));
-    n_amb[i] = A*pow(R_para[i],-k)/M_PRO;
-    B_f[i] = sqrt(32.0*M_PI*eps_B*M_PRO*n_amb[i]*gam_j[i]*(gam_j[i]-1.0))*C;
+    n_amb[i] = A*pow(R_para[i],-k)/M_PRO;   /* in what frame? */
+    B_f[i] = sqrt(32.0*M_PI*eps_B*M_PRO*n_amb[i]*gam_j[i]*(gam_j[i]-1.0))*C; /* in waht frame? */
     gam_e_m[i] = eps_e*(p-2.0)/(p-1.0)*M_PRO/M_ELE*(gam_j[i]-1.0);
     gam_e_c[i] = 6.0*M_PI*M_ELE*C/SIGMA_T/gam_j[i]/B_f[i]/B_f[i]/t_obs[i]*(1.0+z);
   }
@@ -172,7 +172,7 @@ void shock_acc_conical(double k, double A, double z, double theta_j_0, double E_
   fclose(op);
 }
 
-void sync_specpara_conical(double z, double d_l, double k, double p)
+void sync_spec_para_conical(double z, double d_l, double k, double p)
 {
   int i=0;
 
@@ -244,6 +244,70 @@ double sync_spec_conical(double nu, double nu_m, double nu_c, double F_nu_max, d
     
     return F_nu;
 }
+
+/* integration of Eqs. (2) and (4) of Granot, Piran, and Sari 99 */
+/* 先ずは愚直に1000time step分のP_nuとalpha_nuを吐いてみよう。*/
+
+/* Synchrotron emission */
+double syn_func_fit(double x)
+{
+    /* analytical fitting of synchrotron function F(x) */
+    /* see http://arxiv.org/pdf/1301.6908.pdf */
+    
+    double F1 = M_PI*pow(2.0,5.0/3.0)/sqrt(3.0)/GAMMA13*pow(x,1.0/3.0);
+    double F2 = sqrt(M_PI/2.0)*exp(-x)*pow(x,1.0/2.0);
+    
+    double a1_1 = -0.97947838884478688;
+    double a1_2 = -0.83333239129525072;
+    double a1_3 = 0.1554179602681624;
+    double H_1 = a1_1*pow(x,1.0)+a1_2*pow(x,1.0/2.0)+a1_3*pow(x,1.0/3.0);
+    double delta_1 = exp(H_1);
+    
+    double a2_1 = -0.0469247165562628882;
+    double a2_2 = -0.70055018056462881;
+    double a2_3 = 0.0103876297841949544;
+    double H_2 = a2_1*pow(x,1.0)+a2_2*pow(x,1.0/2.0)+a2_3*pow(x,1.0/3.0);
+    double delta_2 = 1.0-exp(H_2);
+    
+    return F1*delta_1+F2*delta_2;
+}
+
+
+void syn_spec(double B, double ne[], double gamma_e[], double dene_e[], double del_ln_gamma_e, double mu[], double del_mu, double gamma_ph[], double P_nu_syn[], double alpha_nu_syn[])
+{
+    int i,j,k;
+    double nu,x,sin_alpha;
+    double integ=0.0;
+    double integ_alpha=0.0;
+    
+    for (k=0;k<N_nph;k++) {
+        nu = gamma_ph[k]*MeC2/H;
+        for (i=0;i<N_ne;i++) {
+            //for (j=1;j<N_mu-1;j++) {
+            /* Do not take summation for j = 0 and j = N_mu where x = ∞ and syn_func = 0 */
+            //sin_alpha = sqrt(1.0-mu[j]*mu[j]);
+            sin_alpha = 2.0/3.0;
+            x= (2.0*M_PI*nu)/(3.0*ELEC*gamma_e[i]*gamma_e[i]*B/2.0/M_ELE/C*sin_alpha); /* Eq. (6.17c) of Rybicki & Lightman */
+            if (i==0 || i==N_ne-1) {
+                //integ += 0.5*sin_alpha*ne[i]*syn_func_fit(x)*del_mu/2.0*gamma_e[i]*del_ln_gamma_e;
+                //integ_alpha += -0.5*sin_alpha*pow(gamma_e[i],2.0)*(-ne[i]/pow(gamma_e[i],2.0))/dene_e[i]*syn_func_fit(x)*del_mu/2.0*gamma_e[i]*del_ln_gamma_e/MeC2;
+                integ += 0.5*sin_alpha*ne[i]*syn_func_fit(x)*gamma_e[i]*del_ln_gamma_e;
+                integ_alpha += -0.5*sin_alpha*pow(gamma_e[i],2.0)*(-ne[i]/pow(gamma_e[i],2.0))/dene_e[i]*syn_func_fit(x)*gamma_e[i]*del_ln_gamma_e/MeC2;
+            } else {
+                //integ += sin_alpha*ne[i]*syn_func_fit(x)*del_mu/2.0*gamma_e[i]*del_ln_gamma_e;
+                //integ_alpha += -sin_alpha*pow(gamma_e[i],2.0)*(ne[i+1]/pow(gamma_e[i+1],2.0)-ne[i]/pow(gamma_e[i],2.0))/dene_e[i]*syn_func_fit(x)*del_mu/2.0*gamma_e[i]*del_ln_gamma_e/MeC2;
+                integ += sin_alpha*ne[i]*syn_func_fit(x)*gamma_e[i]*del_ln_gamma_e;
+                integ_alpha += -sin_alpha*pow(gamma_e[i],2.0)*(ne[i+1]/pow(gamma_e[i+1],2.0)-ne[i]/pow(gamma_e[i],2.0))/dene_e[i]*syn_func_fit(x)*gamma_e[i]*del_ln_gamma_e/MeC2;
+            }
+            //}
+        }
+        P_nu_syn[k] = sqrt(3.0)*pow(ELEC,3.0)*B/MeC2*integ; /* Eq. (6.33) x (2 pi) of Rybicki & Lightman */
+        alpha_nu_syn[k] = C*C/8.0/M_PI/nu/nu*sqrt(3.0)*pow(ELEC,3.0)*B*integ_alpha; /* Eq. (6.52) of Rybicki & Lightman */
+    }
+}
+
+
+
 
 
 void distances(double z, double *d_h, double *d_c, double *d_a, double *d_l)
