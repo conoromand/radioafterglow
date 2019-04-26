81e63590b92fd0698330f9f80df64ba82dd32419
Kazumi Kashiyama <k.kashiyama1120@gmail.com>
2019-04-27 08:25:27 +0900

    clean up

--- radioafterglow/offaxis.c
+++ radioafterglow/offaxis.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-const int n_rbin = 1000;
-
 const double E_c = 4.3e54; /* [erg] */
 const double M_c = 22.0*2e33; /* [g] */
 const double R_c = 2.0e13; /* [cm] */
@@ -29,6 +27,7 @@ const double YR = 365.0*24.0*60.0*60.0; /* [s]  */
 
 const double GAMMA13 = 2.67893; /* Gamma(1/3) */
 
+const int N_tbin = 1000;
 const int N_ne = 512;
 const int N_nph = 512; /* need to be the same as N_ne */
 const int N_mu = 32;
@@ -44,23 +43,23 @@ const double XI_T = 0.24;
 const double POW_ELE = 2.0;
 
 
-void conical_model(double k, double A, double a, double b, double theta_j_0, double gam_j_0);
-void shocked_jet_conical(double k, double A, double theta_j_0, double E_j_0, double eps_B);
+void calc_conical_model(char path[256], double k, double A, double a, double b, double theta_j_0, double gam_j_0);
+void calc_shocked_jet(char path[256], double k, double A, double theta_j_0, double E_j_0, double eps_B);
 void set_var(double mu[], double *del_mu, double ne[], double gam_e[], double dene_e[], double *del_ln_gam_e, double gam_ph[], double dene_ph[], double *del_ln_gam_ph);
 void elec_injection(double t_sh, double n_sh, double B_sh, double gam_e_inj, double gam_e_th, double gam_e_max, double gam_e[], double dne_dt_inj[]);
 double power_ad(double gam_e, double t);
 double power_syn(double gam_e, double B_sh);
 void elec_cooling(double t, double B_sh, double P_cool[], double gam_e[]);
 void elec_time_evolution(double dt, double gam_e[], double dene_e[], double ne_old[], double ne_new[], double dne_dt_inj[], double P_cool[]);
-
-void sync_spec_para_conical(double z, double d_l, double k, double p);
-double sync_spec_conical(double nu, double nu_m, double nu_c, double F_nu_max, double gam_j, double B_f, double R_perp, double p, double z, double d_l);
 double syn_func_fit(double x);
 void syn_spec(double B, double dr, double ne[], double gamma_e[], double dene_e[], double del_ln_gamma_e, double gamma_ph[], double P_nu_syn[], double alpha_nu_syn[]);
 void distances(double z, double *d_h, double *d_c, double *d_a, double *d_l);
 
+
 int main()
 {
+    /* input parameters */
+    /////////////////////////////////////////
     /* Ambient density profile */
     double v_w_str=2.0e8;
     double Mdot_str=1.0e-4*M_SUN/YR;
@@ -68,34 +67,41 @@ int main()
     double A=Mdot_str/4.0/M_PI/v_w_str;
     //double k=0.0;
     //double A=0.1*M_PRO;
-    
-    /* Granot & Piran 2011 */
-    double a=1.0;
-    double b=0.45;
-    
-    /* redshift and distance */
+    double a=1.0; /* see Granot & Piran 2011 */
+    double b=0.45; /* see Granot & Piran 2011 */
+    /* redshift */
     double z = 0.1;
-    double d_h=0.0,d_c=0.0,d_a=0.0,d_l=0.0;
-    distances(z,&d_h,&d_c,&d_a,&d_l);
-    
     /* jet parameters  */
     double theta_j_0=2.0*M_PI*5.0/360.0;
     double gam_j_0=200.0;
     double E_j_0=1.0e55*(1.0-cos(theta_j_0));
-    
     /* shock parameters */
     double eps_B = 0.001;
     double eps_e = 0.01;
     double p = 2.2;
+    /* path */
+    char path[256]="/Users/kakashi/offaxis_data/";
+    /////////////////////////////////////////
     
-    conical_model(k,A,a,b,theta_j_0,gam_j_0);
-    shocked_jet_conical(k,A,theta_j_0,E_j_0,eps_B);
+    
+    /* setting variables */
+    /////////////////////////////////////////
+    double mu[N_mu],ne[N_ne],gam_e[N_ne],dene_e[N_ne],dne_dt_inj[N_ne],P_cool[N_ne],gam_ph[N_nph],dene_ph[N_nph],P_nu_syn[N_nph],alpha_nu_syn[N_nph];
+    double del_mu=0.0,del_ln_gam_e=0.0,del_ln_gam_ph=0.0;
+    double dt=0.0,dr=0.0;
+    set_var(mu,&del_mu,ne,gam_e,dene_e,&del_ln_gam_e,gam_ph,dene_ph,&del_ln_gam_ph);
+    /////////////////////////////////////////
+
+    
+    calc_conical_model(path,k,A,a,b,theta_j_0,gam_j_0);
+    calc_shocked_jet(path,k,A,theta_j_0,E_j_0,eps_B);
+    
     
     int i=0,j;
-    double t_s[n_rbin],t_prime[n_rbin],gam_j[n_rbin],R_perp[n_rbin],R_para[n_rbin],theta_ene_ave[n_rbin],n_f[n_rbin],B_f[n_rbin],gam_e_th[n_rbin],gam_e_inj[n_rbin],gam_e_max[n_rbin];
+    double t_s[N_tbin],t_prime[N_tbin],gam_j[N_tbin],R_perp[N_tbin],R_para[N_tbin],theta_ene_ave[N_tbin],n_f[N_tbin],B_f[N_tbin],gam_e_th[N_tbin],gam_e_inj[N_tbin],gam_e_max[N_tbin];
     FILE *ip;
     char head_ip[256]="shock_acc_conical",dat[256]=".dat",input_file_name[256]={"\0"};
-    sprintf(input_file_name,"%s%s",head_ip,dat);
+    sprintf(input_file_name,"%s%s%s",path,head_ip,dat);
     
     ip = fopen(input_file_name,"r");
     fscanf(ip,"%*[^\n]");
@@ -105,47 +111,55 @@ int main()
     }
     fclose(ip);
 
-    /* setting variables */
-    double mu[N_mu],ne[N_ne],gam_e[N_ne],dene_e[N_ne],dne_dt_inj[N_ne],P_cool[N_ne],gam_ph[N_nph],dene_ph[N_nph],P_nu_syn[N_nph],alpha_nu_syn[N_nph];
-    double del_mu=0.0,del_ln_gam_e=0.0,del_ln_gam_ph=0.0;
-    double dt=0.0,dr=0.0;
-    set_var(mu,&del_mu,ne,gam_e,dene_e,&del_ln_gam_e,gam_ph,dene_ph,&del_ln_gam_ph);
-    
     /* calculate emission intensity */
-    FILE *op;
-    op = fopen("test.dat","w+");
-    for (i=0;i<n_rbin-1;i++) {
-        printf("t = %lf [s]\n",t_s[i]);
+    FILE *op1,*op2;
+    char head1[256]="map_ne",head2[256]="map_pnu",output_file_name1[256]={"\0"},output_file_name2[256]={"\0"};
+    sprintf(output_file_name1,"%s%s%s",path,head1,dat);
+    sprintf(output_file_name2,"%s%s%s",path,head2,dat);
+    op1 = fopen(output_file_name1,"w+");
+    op2 = fopen(output_file_name2,"w+");
+    for (i=0;i<N_tbin-1;i++) {
+        printf("t = %12.3e [s]\n",t_s[i]);
         elec_injection(t_prime[i],n_f[i],B_f[i],gam_e_inj[i],gam_e_th[i],gam_e_max[i],gam_e,dne_dt_inj);
         elec_cooling(t_prime[i],B_f[i],P_cool,gam_e);
         dt = t_prime[i+1]-t_prime[i];
-        dr = (R_para[i+1]-R_para[i])/gam_j[i];
+        dr = (R_para[i+1]-R_para[i])/(4.0*gam_j[i]);
         elec_time_evolution(dt,gam_e,dene_e,ne,ne,dne_dt_inj,P_cool);
         syn_spec(B_f[i],dr,ne,gam_e,dene_e,del_ln_gam_e,gam_ph,P_nu_syn,alpha_nu_syn);
         for (j=0;j<N_ne;j++){
-            fprintf(op,"%le %le %le %le %le %le \n",gam_e[j],dne_dt_inj[j],ne[j],gam_ph[j],P_nu_syn[j],alpha_nu_syn[j]);
+            fprintf(op1,"%le ",ne[j]);
         }
+        fprintf(op1,"\n");
+        for (j=0; j<N_nph; j++) {
+            fprintf(op2,"%le ",P_nu_syn[j]);
+        }
+        fprintf(op2,"\n");
     }
-    fclose(op);
+    fclose(op1);
+    fclose(op2);
+    
+    
+    double d_h=0.0,d_c=0.0,d_a=0.0,d_l=0.0;
+    distances(z,&d_h,&d_c,&d_a,&d_l);
     
     return 0;
 }
 
 
-void conical_model(double k, double A, double a, double b, double theta_j_0, double gam_j_0)
+void calc_conical_model(char path[256], double k, double A, double a, double b, double theta_j_0, double gam_j_0)
 {
     FILE *op;
     char head[256]="conical_model",dat[256]=".dat",output_file_name[256]={"\0"};
-    sprintf(output_file_name,"%s%s",head,dat);
+    sprintf(output_file_name,"%s%s%s",path,head,dat);
     
     int i;
-    double r_s_min=pow((1.0-cos(theta_j_0))*(gam_j_0*gam_j_0-1.0),-1.0/(3.0-k)),r_s_max=1.0e1,del_ln_r_s=(log(r_s_max)-log(r_s_min))/(double)(n_rbin-1),r_s=r_s_min,dr_s=0.0;
+    double r_s_min=pow((1.0-cos(theta_j_0))*(gam_j_0*gam_j_0-1.0),-1.0/(3.0-k)),r_s_max=1.0e1,del_ln_r_s=(log(r_s_max)-log(r_s_min))/(double)(N_tbin-1),r_s=r_s_min,dr_s=0.0;
     double theta_j_tmp=theta_j_0,u_tmp=sqrt(gam_j_0*gam_j_0-1.0);
     double t=r_s_min,dt=0.0,t_s=(1.0-sqrt(1.0-1.0/(u_tmp*u_tmp+1.0)))*t;
     
     op = fopen(output_file_name,"w+");
     fprintf(op,"#k=%le A=%le a=%le b=%le\n",k,A,a,b);
-    for (i=0;i<n_rbin;i++){
+    for (i=0;i<N_tbin;i++){
         r_s = r_s_min*exp(del_ln_r_s*(double)i);
         dr_s = r_s*(exp(del_ln_r_s)-1.0);
         dt = dr_s/sqrt(1.0-1.0/(u_tmp*u_tmp+1.0));
@@ -164,15 +178,15 @@ void conical_model(double k, double A, double a, double b, double theta_j_0, dou
 }
 
 
-void shocked_jet_conical(double k, double A, double theta_j_0, double E_j_0, double eps_B)
+void calc_shocked_jet(char path[256], double k, double A, double theta_j_0, double E_j_0, double eps_B)
 {
     int i=0;
     
     /* reading the input file of the jet dynamics */
-    double r[n_rbin],t[n_rbin],t_s[n_rbin],t_prime[n_rbin],theta_j[n_rbin],gam_j[n_rbin];
+    double r[N_tbin],t[N_tbin],t_s[N_tbin],t_prime[N_tbin],theta_j[N_tbin],gam_j[N_tbin];
     FILE *ip;
     char head_ip[256]="conical_model",dat[256]=".dat",input_file_name[256]={"\0"};
-    sprintf(input_file_name,"%s%s",head_ip,dat);
+    sprintf(input_file_name,"%s%s%s",path,head_ip,dat);
     
     ip = fopen(input_file_name,"r");
     fscanf(ip,"%*[^\n]");
@@ -181,12 +195,15 @@ void shocked_jet_conical(double k, double A, double theta_j_0, double E_j_0, dou
     }
     fclose(ip);
     
-    /* calculating the microphysics parameters at the shock */
-    double R_para[n_rbin],R_perp[n_rbin],theta_ene_ave[n_rbin];
-    double n_amb,n_f[n_rbin],B_f[n_rbin],gam_e_inj[n_rbin],gam_e_max[n_rbin],gam_e_th[n_rbin];
+    double R_para[N_tbin],R_perp[N_tbin],theta_ene_ave[N_tbin];
     double R_j = pow((3.0-k)*E_j_0/2.0/M_PI/A/C/C,1.0/(3.0-k));
+    double n_amb;
     
-    for (i=0;i<n_rbin;i++){
+    
+    /* calculating the physical parameters at the (forward) shock downstream*/
+    double n_f[N_tbin],B_f[N_tbin],gam_e_inj[N_tbin],gam_e_max[N_tbin],gam_e_th[N_tbin];
+    
+    for (i=0;i<N_tbin;i++){
         t[i] = R_j*t[i]/C;
         t_s[i] = R_j*t_s[i]/C; /* source rest frame */
         t_prime[i] = t_s[i]/gam_j[i]; /* jet rest frame */
@@ -204,10 +221,10 @@ void shocked_jet_conical(double k, double A, double theta_j_0, double E_j_0, dou
     /* outputting the microphysics parameters at the shock */
     FILE *op;
     char head_op[256]="shock_acc_conical",output_file_name[256]={"\0"};
-    sprintf(output_file_name,"%s%s",head_op,dat);
+    sprintf(output_file_name,"%s%s%s",path,head_op,dat);
     op = fopen(output_file_name,"w+");
     fprintf(op,"# t_s[s], t_prime[s], gam_j, R_perp[cm], R_para[cm], theta_j, n_f[cm^-3], B_f[G], gam_e_th, gam_e_inj, gam_e_max \n");
-    for (i=0;i<n_rbin;i++){
+    for (i=0;i<N_tbin;i++){
         fprintf(op,"%le %le %le %le %le %le %le %le %le %le %le \n",
                 t_s[i],t_prime[i],gam_j[i],R_perp[i],R_para[i],theta_ene_ave[i],n_f[i],B_f[i],gam_e_th[i],gam_e_inj[i],gam_e_max[i]);
     }
@@ -215,15 +232,12 @@ void shocked_jet_conical(double k, double A, double theta_j_0, double E_j_0, dou
 }
 
 
-/* integration of Eqs. (2) and (4) of Granot, Piran, and Sari 99 */
-/* 先ずは愚直に1000time step分のP_nuとalpha_nuを吐いてみよう。*/
 void set_var(double mu[], double *del_mu, double ne[], double gam_e[], double dene_e[], double *del_ln_gam_e, double gam_ph[], double dene_ph[], double *del_ln_gam_ph)
 {
     /* setting basic variables as strings */
     
-    int i;
-    
     /* electron energy in unit of MeC2 */
+    int i;
     double del_ln_gam_e_tmp = (log(GAMMA_ELEC_MAX)-log(GAMMA_ELEC_MIN))/(double)(N_ne-1);
     for (i=0;i<N_ne;i++){
         gam_e[i] = GAMMA_ELEC_MIN*exp(del_ln_gam_e_tmp*(double)i);
@@ -411,52 +425,4 @@ void distances(double z, double *d_h, double *d_c, double *d_a, double *d_l)
   *d_c = d_c_tmp*PC;
   *d_a = d_a_tmp*PC;
   *d_l = d_l_tmp*PC;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-double sync_spec_conical(double nu, double nu_m, double nu_c, double F_nu_max, double gam_j, double B_f, double R_perp, double p, double z, double d_l)
-{
-    double gam_e_char_obs,F_nu_BB_crit,F_nu;
-    gam_e_char_obs = sqrt(2.0*M_PI*M_ELE*C*(1.0+z)*nu/gam_j/ELEC/B_f);
-    F_nu_BB_crit = 2.0*M_PI*nu*nu*gam_j*gam_e_char_obs*M_ELE*pow(R_perp/d_l,2.0)*pow(1.0+z,3.0);
-    
-    if (nu_m <= nu_c){
-        
-        if (nu <= nu_m)
-        F_nu = pow(nu/nu_m,1.0/3.0)*F_nu_max;
-        else if (nu <= nu_c)
-        F_nu = pow(nu/nu_m,-(p-1.0)/2.0)*F_nu_max;
-        else
-        F_nu = pow(nu_c/nu_m,-(p-1.0)/2.0)*pow(nu/nu_c,-p/2.0)*F_nu_max;
-        
-        if (F_nu >= F_nu_BB_crit)
-        F_nu = F_nu_BB_crit;
-        
-    } else {
-        
-        if (nu <= nu_c)
-        F_nu = pow(nu/nu_c,1.0/3.0)*F_nu_max;
-        else if (nu <= nu_m)
-        F_nu = pow(nu/nu_c,-1.0/2.0)*F_nu_max;
-        else
-        F_nu = pow(nu_m/nu_c,-1.0/2.0)*pow(nu/nu_m,-p/2.0)*F_nu_max;
-        
-        if (F_nu >= F_nu_BB_crit)
-        F_nu = F_nu_BB_crit;
-        
-    }
-    
-    return F_nu;
 }
